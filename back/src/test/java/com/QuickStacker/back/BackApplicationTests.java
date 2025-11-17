f92df5cd4425a96d099fd1696c5cf9d27b51f77d
Gabriel Esmeraldo <162829471+gabEsmeraldo@users.noreply.github.com>
2025-11-17 14:00:44 -0300

    Add files via upload

---
+++ back/src/test/java/com/QuickStacker/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuickStacker.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
