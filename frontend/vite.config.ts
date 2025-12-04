bd3a7af34f4735f228faf5e8f7d07b263cccc33c
Gabriel Esmeraldo <162829471+gabEsmeraldo@users.noreply.github.com>
2025-12-04 01:25:36 -0300

    Update API proxy target to production server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8081',
+        target: 'quickstacker-production.up.railway.app',
         changeOrigin: true,
         secure: false,
       },
